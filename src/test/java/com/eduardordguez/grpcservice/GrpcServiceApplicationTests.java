9d7a178d02c30f05054f2f9e7bef1da29133bbfa
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2024-06-07 18:56:44 -0600

    add recommended spring grpc dependencies for simplifying the definition via annotations

--- src/test/java/com/eduardordguez/grpcservice/GrpcServiceApplicationTests.java
+++ src/test/java/com/eduardordguez/grpcservice/GrpcServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eduardordguez.grpc_service;
+package com.eduardordguez.grpcservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GrpcServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
